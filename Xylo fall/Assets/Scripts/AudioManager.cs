7bc5ded0a7e563fac488dc355551506623370c4d
YB912 <YB912TU@gmail.com>
2023-06-10 21:11:20 +0430

    Add a new Xylophone note and clean up, fix and modify some prefabs, scripts, etc.

--- Xylo fall/Assets/Scripts/AudioManager.cs
+++ Xylo fall/Assets/Scripts/AudioManager.cs
@@ -3,22 +3,28 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public Sound[] Sounds;
 
-    public Sound[] sounds;
-
-    public void PlaySound(string name, float volume, float pitch)
-    {
-        Sound sound = Array.Find(sounds, s => s.Name == name);
-        sound.Source.volume = volume;
-        sound.Source.pitch = pitch;
-        sound.Source.Play();
-    }
     private void Awake()
     {
-        foreach (Sound sound in sounds)
+        foreach (Sound sound in Sounds)
         {
             sound.Source = gameObject.AddComponent<AudioSource>();
             sound.Source.clip = sound.Clip;
         }
+    }
+    public void PlaySound(string name, float volume, float pitch)
+    {
+        Sound sound = Array.Find(Sounds, s => s.Name == name);
+        sound.Source.volume = volume;
+        sound.Source.pitch = pitch;
+        sound.Source.Play();
+    }
+    public void PlaySound(int index, float volume, float pitch)
+    {
+        Sound sound = Sounds[index];
+        sound.Source.volume = volume;
+        sound.Source.pitch = pitch;
+        sound.Source.Play();
     }
 }
--- Xylo fall/Assets/Scripts/BarsCollisionActions.cs
+++ Xylo fall/Assets/Scripts/BarsCollisionActions.cs
@@ -1,18 +1,17 @@
-using System;
 using UnityEngine;
 
 public class BarsCollisionActions : MonoBehaviour
 {
-    public BarColor color;
-
     private const float MINIMUMVELOCITY = 8f; // magnitude of minimum velocity each ball should have in order to make a sound
-    private AudioManager m_AudioManager;
-    private ParticleSystem m_ParticleSystem;
+
+    public BarColor color;
+    public GameObject NoteParticles;
+
+    private AudioManager _audioManager;
 
     private void Awake()
     {
-        m_AudioManager = FindObjectOfType<AudioManager>();
-        m_ParticleSystem = GetComponentInChildren<ParticleSystem>();
+        _audioManager = FindObjectOfType<AudioManager>();
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -24,14 +23,13 @@ public class BarsCollisionActions : MonoBehaviour
             if (volume <  0) { volume = 0; }
             else if (volume > 1) {  volume = 1; }
 
-            m_AudioManager.PlaySound(Array.Find(m_AudioManager.sounds, s => s.Name == "bar" + color.ToString()).Name, volume, 1);
-            m_ParticleSystem.transform.position = collision.contacts[0].point;
-            m_ParticleSystem.Play();
+            _audioManager.PlaySound("bar" + color.ToString(), volume, 1);
+            var Notes = Instantiate(NoteParticles, collision.contacts[0].point, Quaternion.identity);
+            Notes.GetComponent<ParticleSystem>().Play();
         }
     }
 }
-
 public enum BarColor
 {
-    C,D,E,F,G,A,B
+    C,D,E,F,G,A,B,C2
 }
